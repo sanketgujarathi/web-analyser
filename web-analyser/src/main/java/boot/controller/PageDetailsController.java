06bf699aa14bfb5df8f8387922e9b3e69843039c
Sanket Gujarathi <sanketgujarathi@gmail.com>
2017-04-17 02:29:03 +0530

    Implemented link anlyser functionality

--- web-analyser/src/main/java/boot/controller/PageDetailsController.java
+++ web-analyser/src/main/java/boot/controller/PageDetailsController.java
@@ -3,6 +3,7 @@ package boot.controller;
 import boot.model.PageDetails;
 import boot.service.PageDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotNull;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by sanketg on 4/14/2017.
  */
 @RestController
+@Validated
 public class PageDetailsController {
 
     @Autowired
@@ -23,16 +26,18 @@ public class PageDetailsController {
 
     @RequestMapping(value="/pageDetails", method= RequestMethod.GET,
             produces="application/json")
-    public PageDetails getPageDetail(@RequestParam @NotNull String url) {
-        try {
-            final PageDetails htmlPageDetails = pageDetailService.getPageDetails(url);
-            return htmlPageDetails;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return new PageDetails();
+    public PageDetails getPageDetail(@RequestParam @NotNull String url) throws IOException {
+            return  pageDetailService.getPageDetails(url);
     }
+
+    @RequestMapping(value="/linkDetails", method= RequestMethod.GET,
+            produces="application/json")
+    public Map<String, Boolean> getLinkDetail(@RequestParam @NotNull String url) throws IOException {
+            return pageDetailService.getLinkDetails(url);
+
+    }
+
+
 
 
 }
--- web-analyser/src/main/java/boot/service/PageDetailService.java
+++ web-analyser/src/main/java/boot/service/PageDetailService.java
@@ -1,17 +1,18 @@
 package boot.service;
 
+import boot.dao.PageDao;
 import boot.model.PageDetails;
 import com.google.common.net.InternetDomainName;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.DocumentType;
 import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.net.*;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -20,9 +21,11 @@ import java.util.stream.Collectors;
 @Service
 public class PageDetailService {
 
+    @Autowired
+    private PageDao pageDao;
 
     public PageDetails getPageDetails(String url) throws IOException {
-        final Document document = Jsoup.connect(url).get();
+        final Document document = pageDao.getPage(url);
         final PageDetails htmlPageDetails = new PageDetails();
         htmlPageDetails.setTitle(document.title());
         htmlPageDetails.setHtmlVersion(getHtmlVersion(document));
@@ -31,6 +34,7 @@ public class PageDetailService {
         htmlPageDetails.setContainsLogin(containsLoginForm(document));
         return htmlPageDetails;
     }
+
 
     private String getDomain(final Document document) {
         URI uri = null;
@@ -48,7 +52,7 @@ public class PageDetailService {
         return document
                 .body()
                 .select("a[href]")
-                .stream()
+                .parallelStream()
                 .map(e -> e.attr("href"))
                 .collect(Collectors.groupingBy(e -> pattern.matcher(e).matches() ? "Internal" : "External", Collectors.summingInt(e -> 1)));
     }
@@ -76,4 +80,43 @@ public class PageDetailService {
         String htmlVersion = documentType.attr("publicid");
         return "".equals(htmlVersion) ? "HTML5" : htmlVersion;
     }
+
+    public Map<String, Boolean> getLinkDetails(String url) throws IOException, MalformedURLException {
+        final Document document = pageDao.getPage(url);
+        return document
+                .select("a[href]")
+                .parallelStream()
+                .map(p -> {
+                    try {
+                        return new URL(p.attr("href"));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                })
+                .filter( p -> p!=null)
+                .collect(Collectors.toMap(
+                p -> p.toString(),
+                p -> {
+                    try {
+                        URLConnection urlConnection = p.openConnection();
+                        if (urlConnection instanceof HttpsURLConnection) {
+                            return true;
+                        }
+                        else if (urlConnection instanceof HttpURLConnection) {
+                            return false;
+                        }
+                    } catch (IOException e) {
+                            return false;
+                    }
+                    return false;
+                },
+                (ssl1, ssl2) -> ssl1 || ssl2
+
+
+        ));
+
+    }
+
+
 }
